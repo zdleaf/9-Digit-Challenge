924bd3eb3171a30d42ac8ebd1202e20069e49d89
zdleaf <leaf@zincldn.co.uk>
2019-01-31 12:40:07 +0000

    remove old code

--- 9_digit_challenge.py
+++ 9_digit_challenge.py
@@ -91,7 +91,6 @@ for x in potentials:
 	workingPosition = 1
 	potentials = CalculatePotentials(workingArray, workingPosition)
 	print("calculating for {}, potentials: {}".format(x, potentials))
-	#print("potentials 2: ", potentials)
 
 # need to loop all and tree this off 
 # for x in potentials
@@ -101,24 +100,4 @@ print("----------")
 print("calcPosition: ", calcPosition)
 # print("working array: ", workingArray)
 # print("temp potentials: ", temporaryPotentials)
-
-
-
-
-
-
-
-# rule 1: Each digit cannot equal it's position plus or minus 1
-# note: position 1 is first position aka, indexed by 0 in array_rule1, therefore for clarity we refer to position and possibilities as xp and yp for comparison against the rule
-
-arrayRule1 = np.zeros(shape=[9, 9], dtype=int) # initialise 9x9 integer array with 0's
-
-for x in range(0, 9): 	# position 1-9 (indexed as 0-8)
-	xp = x+1 			# xp: refer to index 0 as position 1
-	for y in range(0, 9): # possibilities i.e. digits 1-9
-		yp = y+1		# yp: first possibility should be 1, not the index 0
-		if yp != xp and yp != xp+1 and yp != xp-1:	# if possibility is possible as per rule 1
-			arrayRule1[x,y] = yp
-
-# print(arrayRule1)
 
